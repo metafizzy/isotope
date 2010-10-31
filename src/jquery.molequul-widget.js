328f3086f0eb3f61fd79be63007d8a23441552a4
David DeSandro <david@desandro.com>
2010-10-31 10:05:03 -0400

    clean up setupAtoms, addAtoms, insert methods

--- src/jquery.molequul-widget.js
+++ src/jquery.molequul-widget.js
@@ -78,16 +78,14 @@
       
       // sorting
       var originalOrderSorter = {
-        'original-order' : function( $elem ) {
-          return this.elemCount;
+        'original-order' : function( $elem, instance ) {
+          return instance.elemCount;
         }
       };
 
-      this.options.getSortData = $.extend( originalOrderSorter, this.options.getSortData );
-
+      this.options.getSortData = $.extend( this.options.getSortData, originalOrderSorter );
 
       this._setupAtoms( this.atoms.$all );
-      this.atoms.$all.molequul( '_setupAtoms' );
       
       
       // get top left position of where the bricks should be
@@ -103,14 +101,9 @@
         instance.$elem.addClass( instance.options.containerClass );
       }, 0 );
       
-      // console.log( this.options.layoutMode )
-      
       // do any layout-specific setup
       this.width = this.$elem.width();
       this._getMasonryColCount();
-      
-      // save data
-      // this.data( 'molequul', props );
       
       // bind resize method
       if ( this.options.resizeable ) {
@@ -130,8 +123,6 @@
     // after it has already been initialized.
     _init : function( callback ) {
       
-      // this.options = $.extend( true, Molequul.options, this.options );
-
       // check if watched properties are new
       var instance = this;
       $.each( [ 'filter', 'sortBy', 'sortDir' ], function( i, propName ){
@@ -181,21 +172,7 @@
     // ====================== Adding ======================
     
     _addSortData : function( $atoms ) {
-      var instance = this;
-      $atoms.each(function(){
-        var $this = $(this),
-            sortData = {},
-            getSortData = instance.options.getSortData,
-            key;
-        // get value for sort data based on fn( $elem ) passed in
-        for ( key in getSortData ) {
-          sortData[ key ] = getSortData[ key ]( $this );
-        }
-        // apply sort data to $element
-        $this.data( 'molequul-sort-data', sortData );
-        // increment element count
-        instance.elemCount ++;
-      });
+
     },
     
     _setupAtoms : function( $atoms ) {
@@ -209,8 +186,22 @@
 
       $atoms.css( atomStyle );
       
-      // add sort data to each elem
-      this._addSortData( $atoms );
+      var instance = this;
+      $atoms.each(function(){
+        var $this = $(this),
+            sortData = {},
+            getSortData = instance.options.getSortData,
+            key;
+        // get value for sort data based on fn( $elem ) passed in
+        for ( key in getSortData ) {
+          sortData[ key ] = getSortData[ key ]( $this, instance );
+        }
+        // apply sort data to $element
+        $this.data( 'molequul-sort-data', sortData );
+        // increment element count
+        // console.log( instance.elemCount )
+        instance.elemCount ++;
+      });
 
       // return this.molequul( 'addSortData', props ).css( atomStyle );
     },
@@ -513,7 +504,8 @@
     
     
     reLayout : function( callback ) {
-      this
+      console.log('relaying out', this.atoms.$filtered.length )
+      return this
         [ '_' +  this.options.layoutMode + 'ResetLayoutProps' ]()
         .layout( this.atoms.$filtered, callback )
     },
@@ -521,19 +513,26 @@
     // ====================== Convenience methods ======================
     
     // adds a jQuery object of items to a molequul container
-    add : function( $content ) {
+    addAtoms : function( $content ) {
       var $newAtoms = this._filterFind( $content, this.options.itemSelector );
       this._setupAtoms( $newAtoms );
       // add new atoms to atoms pools
       this.atoms.$all = this.atoms.$all.add( $newAtoms );
-      this.atoms.$filtered = this.atoms.$filtered.add( $newAtoms );
-      return this;
+      // this.atoms.$filtered = this.atoms.$filtered.add( $newAtoms );
+      return $newAtoms;
     },
     
     // convienence method for adding elements properly to any layout
-    insert : function( $content ) {
+    insert : function( $content, callback ) {
       this.$elem.append( $content );
-      return this.add( $content )._init();
+      
+      var $newAtoms = this.addAtoms( $content ),
+          $filteredAtoms = this._filter( $newAtoms );
+
+      this.atoms.$filtered = this.atoms.$filtered.add( $filteredAtoms );
+
+      this._sort().reLayout( callback );
+      
     },
     
     // convienence method for working with Infinite Scroll
