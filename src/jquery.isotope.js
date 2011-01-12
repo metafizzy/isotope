fa3fe9df9fe1dd6ebddc66708adee75747e78179
David DeSandro <david@desandro.com>
2011-01-12 08:24:34 -0500

    src : add imagesLoaded plugin by Paul Irish

--- src/jquery.isotope.js
+++ src/jquery.isotope.js
@@ -886,6 +886,35 @@
   
   
 
+  // mit license. paul irish. 2010.
+  // webkit fix from Oren Solomianik. thx!
+
+  // callback function is passed the last image to load
+  //   as an argument, and the collection as `this`
+
+
+  $.fn.imagesLoaded = function(callback){
+    var elems = this.find('img'),
+        len   = elems.length,
+        _this = this;
+
+    elems.bind('load',function(){
+      if (--len <= 0){ 
+        callback.call( _this ); 
+      }
+    }).each(function(){
+      // cached images don't fire load sometimes, so we reset src.
+      if (this.complete || this.complete === undefined){
+        var src = this.src;
+        // webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
+        // data uri bypasses webkit log warning (thx doug jones)
+        this.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        this.src = src;
+      }  
+    }); 
+
+    return this;
+  };
   
 
   
