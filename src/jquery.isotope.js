12f0593d19cd8410b211208c1d489ab7a991213a
David DeSandro <david@desandro.com>
2011-01-12 08:24:49 -0500

    src : typo in error message

--- src/jquery.isotope.js
+++ src/jquery.isotope.js
@@ -451,7 +451,7 @@
       
       // if colW == 0, back out before divide by zero
       if ( !this[ namespace ][ measure ] ) {
-        $.error( measure + 'calculated to be zero. Stopping Isotope plugin before divide by zero. Check that the width of first child inside the isotope container is not zero.')
+        $.error( measure + ' calculated to be zero. Stopping Isotope plugin before divide by zero. Check that the width of first child inside the isotope container is not zero.')
         return this;
       }
       this[ size ] = this.element[ size ]();
