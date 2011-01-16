661945c7286073fdea4b35d8400e3f18ce65392b
David DeSandro <david@desandro.com>
2011-01-16 12:59:45 -0500

    docs : remove console.logs

--- _includes/add-buttons.js
+++ _includes/add-buttons.js
@@ -1,16 +1,11 @@
       $('#insert a').click(function(){
         var i = Math.ceil( Math.random()*3 + 1 ),
             newEls = '';
-        // window.console && console.log( i )
         while ( i-- ) {
           newEls += fakeElement.create();
         }
         var $newEls = $( newEls )
-        // $container.append( $newEls ).isotope( 'appendToLayout', $newEls );
         $container.isotope( 'insert', $newEls );
-        // $container.append( $newEls ).isotope( 'addAtoms', $newEls ).isotope();
-        // $container.isotope()
-        // console.log( next )
 
         return false;
       });
--- _includes/sort-buttons.js
+++ _includes/sort-buttons.js
@@ -5,12 +5,9 @@
         var $this = $(this),
             sortName = $this.attr('href').slice(1),
             asc = $this.parents('.sort').hasClass('asc');
-        // var sortTest = 'sort ' + sortName + ' ' + direction;
-        // console.time( sortTest );
         $container.isotope({ 
           sortBy : sortName,
           sortAscending : asc
         });
-        // console.timeEnd( sortTest );
         return false;
       });
