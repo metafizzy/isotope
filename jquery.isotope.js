a2a238968347199842dd7e2d552741d5a63c90b9
David DeSandro <david@desandro.com>
2011-05-03 17:28:32 -0400

    src : convert isoTransform to a bunch of vars

--- jquery.isotope.js
+++ jquery.isotope.js
@@ -50,6 +50,8 @@
     return getStyleProperty;
   }());
 
+  var transformProp = getStyleProperty('transform');
+
   // ========================= miniModernizr ===============================
   // <3<3<3 and thanks to Faruk and Paul for doing the heavy lifting
 
@@ -78,7 +80,7 @@
         {
           name : 'csstransforms',
           getResult : function() {
-            return !!getStyleProperty('transform');
+            return !!transformProp;
           }
         },
         {
@@ -155,16 +157,16 @@
   // ========================= isoTransform ===============================
 
   /**
-   *  provides hooks for .css({ scale: value, translate: x y })
+   *  provides hooks for .css({ scale: value, translate: [x, y] })
    *  Progressively enhanced CSS transforms
    *  Uses hardware accelerated 3D transforms for Safari
    *  or falls back to 2D transforms.
    */
-  var isoTransform = {
+  
+  if ( Modernizr.csstransforms ) {
     
-    transformProp : getStyleProperty('transform'),
-    
-    fnUtils : Modernizr.csstransforms3d ? 
+        // i.e. transformFnNotations.scale(0.5) >> 'scale3d( 0.5, 0.5, 1)'
+    var transformFnNotations = Modernizr.csstransforms3d ? 
       { // 3D transform functions
         translate : function ( position ) {
           return 'translate3d(' + position[0] + 'px, ' + position[1] + 'px, 0) ';
@@ -177,19 +179,20 @@
         translate : function ( position ) {
           return 'translate(' + position[0] + 'px, ' + position[1] + 'px) ';
         },
-        scale :  function ( scale ) {
+        scale : function ( scale ) {
           return 'scale(' + scale + ') ';
         }
       }
-    ,
-    
-    set : function( elem, name, value ) {
+    ;
+
+    function setIsoTransform ( elem, name, value ) {
       var $elem = $(elem),
           // unpack current transform data
           data =  $elem.data('isoTransform') || {},
           newData = {},
           fnName,
-          transformObj = {};
+          transformObj = {},
+          transformValue;
 
       // i.e. newData.scale = 0.5
       newData[ name ] = value;
@@ -197,9 +200,8 @@
       $.extend( data, newData );
 
       for ( fnName in data ) {
-        var transformValue = data[ fnName ],
-            getFn = isoTransform.fnUtils[ fnName ];
-        transformObj[ fnName ] = getFn( transformValue );
+        transformValue = data[ fnName ];
+        transformObj[ fnName ] = transformFnNotations[ fnName ]( transformValue );
       }
 
       // get proper order
@@ -214,69 +216,68 @@
       $elem.data( 'isoTransform', data );
 
       // set name to vendor specific property
-      elem.style[ isoTransform.transformProp ] = valueFns;
-
+      elem.style[ transformProp ] = valueFns;
     }
-  };
+   
+    // ==================== scale ===================
   
-  // ==================== scale ===================
+    $.cssNumber.scale = true;
   
-  $.cssNumber.scale = true;
-  
-  $.cssHooks.scale = {
-    set: function( elem, value ) {
+    $.cssHooks.scale = {
+      set: function( elem, value ) {
 
-      if ( typeof value === 'string' ) {
-        value = parseFloat( value );
+        if ( typeof value === 'string' ) {
+          value = parseFloat( value );
+        }
+
+        setIsoTransform( elem, 'scale', value );
+
+      },
+      get: function( elem, computed ) {
+        var transform = $.data( elem, 'isoTransform' );
+        return transform && transform.scale ? transform.scale : 1;
       }
+    };
 
-      isoTransform.set( elem, 'scale', value );
-
-    },
-    get: function( elem, computed ) {
-      var transform = $.data( elem, 'transform' );
-      return transform && transform.scale ? transform.scale : 1;
-    }
-  };
-
-  $.fx.step.scale = function( fx ) {
-    $.cssHooks.scale.set( fx.elem, fx.now+fx.unit );
-  };
+    $.fx.step.scale = function( fx ) {
+      $.cssHooks.scale.set( fx.elem, fx.now+fx.unit );
+    };
   
   
-  // ==================== translate ===================
+    // ==================== translate ===================
     
-  $.cssNumber.translate = true;
+    $.cssNumber.translate = true;
   
-  $.cssHooks.translate = {
-    set: function( elem, value ) {
+    $.cssHooks.translate = {
+      set: function( elem, value ) {
 
-      // all this would be for public ease-of-use,
-      // but we're leaving it out since this add-on is
-      // only for internal-plugin use
-      // if ( typeof value === 'string' ) {
-      //   value = value.split(' ');
-      // }
-      // 
-      //  
-      // var i, val;
-      // for ( i = 0; i < 2; i++ ) {
-      //   val = value[i];
-      //   if ( typeof val === 'string' ) {
-      //     val = parseInt( val );
-      //   }
-      // }
+        // all this would be for public ease-of-use,
+        // but we're leaving it out since this add-on is
+        // only for internal-plugin use
+        // if ( typeof value === 'string' ) {
+        //   value = value.split(' ');
+        // }
+        // 
+        //  
+        // var i, val;
+        // for ( i = 0; i < 2; i++ ) {
+        //   val = value[i];
+        //   if ( typeof val === 'string' ) {
+        //     val = parseInt( val );
+        //   }
+        // }
 
-      isoTransform.set( elem, 'translate', value );
+        setIsoTransform( elem, 'translate', value );
 
-    },
+      },
     
-    get: function( elem, computed ) {
-      var transform = $.data( elem, 'transform' );
-      return transform && transform.translate ? transform.translate : [ 0, 0 ];
-    }
-  };
+      get: function( elem, computed ) {
+        var transform = $.data( elem, 'isoTransform' );
+        return transform && transform.translate ? transform.translate : [ 0, 0 ];
+      }
+    };
 
+  }
 
 
 
@@ -728,7 +729,7 @@
           this.style.left = null;
           this.style.opacity = null;
           if ( usingTransforms ) {
-            this.style[ isoTransform.transformProp ] = null;
+            this.style[ transformProp ] = null;
           }
         });
       
