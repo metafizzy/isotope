829eaeadc6434e4d313b627ccff4e74c759a1051
David DeSandro <desandrocodes@gmail.com>
2014-03-31 07:55:32 -0400

    remove old files from v1

--- minify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# minifies jquery.isotope.js
-# requires nodejs & uglify-js
-
-IN=jquery.isotope.js
-OUT=jquery.isotope.min.js
-
-uglifyjs $IN --compress conditionals=true --mangle --comments --output $OUT
