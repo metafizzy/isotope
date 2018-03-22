ab20d547d09e81dc5fa9609fa977124ce3086755
Faison Zutavern <faison@reaktivstudios.com>
2018-03-22 18:13:50 -0500

    Dispatch an event with all the filtered Items

--- dist/isotope.pkgd.js
+++ dist/isotope.pkgd.js
@@ -3177,6 +3177,7 @@ var trim = String.prototype.trim ?
     var matches = [];
     var hiddenMatched = [];
     var visibleUnmatched = [];
+    var results;
 
     var test = this._getFilterTest( filter );
 
@@ -3201,12 +3202,17 @@ var trim = String.prototype.trim ?
       }
     }
 
-    // return collections of items to be manipulated
-    return {
+    results = {
       matches: matches,
       needReveal: hiddenMatched,
       needHide: visibleUnmatched
     };
+
+    // allow the filtered items to be modified
+    this.dispatchEvent( 'itemsFiltered', null, [ results ] );
+
+    // return collections of items to be manipulated
+    return results;
   };
 
   // get a jQuery, function, or a matchesSelector test given the filter
@@ -3558,4 +3564,3 @@ var trim = String.prototype.trim ?
   return Isotope;
 
 }));
-
